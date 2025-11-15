7446caa64fe61f557beffe79983e96b35ddf286f
eduplay1216-alt <eduplay1216@icloud.com>
2025-11-15 01:10:59 -0300

    Updated GoogleCalendarAuth.tsx

--- components/GoogleCalendarAuth.tsx
+++ components/GoogleCalendarAuth.tsx
@@ -137,13 +137,20 @@ export const GoogleCalendarAuth: React.FC<GoogleCalendarAuthProps> = ({ onAuthCh
         </div>
       ) : (
         <button
-          onClick={handleSignIn}
-          className="flex items-center space-x-2 bg-blue-600 hover:bg-blue-700 text-white px-4 py-2 rounded-lg transition-colors text-sm"
-        >
-          <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-            <path fillRule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clipRule="evenodd" />
-            <span>Conectar ao Google Calendar</span>
-        </button>
+  onClick={handleSignIn}
+  className="flex items-center space-x-2 bg-blue-600 hover:bg-blue-700 text-white px-4 py-2 rounded-lg transition-colors text-sm"
+>
+  <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+    <path
+      fillRule="evenodd"
+      d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z"
+      clipRule="evenodd"
+    />
+  </svg>
+
+  <span>Conectar ao Google Calendar</span>
+</button>
+
       )}
     </div>
   );
